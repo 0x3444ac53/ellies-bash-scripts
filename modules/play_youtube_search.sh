4829fd028a3846deaf6da1308e88face68f8c136
Ellie Mae Galyean <elliemaegalyean@gmail.com>
2019-10-27 14:10:00 -0400

    Made it quiet

--- modules/play_youtube_search.sh
+++ modules/play_youtube_search.sh
@@ -1,7 +1,7 @@
 play_youtube_audio(){
-    mpv --no-video ${$(youtube-dl -g ytsearch1:"$1")[2]}
+    mpv --no-video ${$(youtube-dl -g ytsearch1:"$1" 2>/dev/null)[2]}
 }
 play_youtube_video(){
-    info=("${(@f)$(youtube-dl -g ytsearch":$1")}")
+    info=("${(@f)$(youtube-dl -g ytsearch":$1" 2>/dev/null)}")
     mpv "${info[1]}" --audio-file="${info[2]}"
 }
