d50d57559da74307050b888f64cb07cdf3642406
Ellie Mae Galyean <elliemaegalyean@gmail.com>
2020-01-19 18:40:59 -0500

    better search module

--- modules/isenv.sh
+++
@@ -1,4 +0,0 @@
-
-isvenv(){
-    return $(python -c "import sys; exit(not ((hasattr(sys, 'real_prefix') or (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))))")
-}
--- modules/search.sh
+++ modules/search.sh
@@ -1,27 +1,25 @@
-search(){
-    url='https://google.com/search?q=%s'
-    searchoptions=''
-    if [ "${1:0:1}" = "-" ]; then
+function search {
+    local searchTerm
+    local site
+    site="https://google.com/search?q="
+    if [[ "$1" == -* ]]; then
         case $1 in
-            "-y")
-                url='https://www.youtube.com/results?q=%s'
-                shift
+            "-y"|"--youtube")
+                site="https://www.youtube.com/results?search_query="
                 ;;
-            "-r")
-                searchoptions="+site:www.reddit.com"
-                shift
+            "-g"|"--google")
+                site="https://www.google.com/search?q="
                 ;;
-            "-w")
-                url='https://en.wikipedia.org/w/index.php?search=%s'
-                shift
+            *)
+                echo "unrecognised "$1""
                 ;;
-            "-od")
-                searchoptions='+intitle%3A%22index+of%22+-inurl%3A%28jsp%7Cpl%7Cphp%7Chtml%7Caspx%7Chtm%7Ccf%7Cshtml%29+-inurl%3A%28hypem%7Cunknownsecret%7Csirens%7Cwriteups%7Ctrimediacentral%7Carticlescentral%7Clisten77%7Cmp3raid%7Cmp3toss%7Cmp3drug%7Ctheindexof%7Cindex_of%7Cwallywashis%7Cindexofmp3%29'
-                shift
-                 ;;
         esac
+        shift
     fi
-    echo "$*" > searchy
-    firefox "$(printf "$url" "$(sed -e 's/ /+/g' searchy)")$searchoptions" 2>/dev/null &
+    if [[ "$*" == "" ]]; then
+        searchTerm=$(dmenu -p "Search: " < <())
+    else
+        searchTerm="$*"
+    fi
+    firefox "$(printf "${site}%s" "$(echo "$searchTerm" | tr ' ' '+')")" &
 }
-
